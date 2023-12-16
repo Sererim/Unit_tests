775f05575ddbf5faee91bd36cd6dc9c44f78aa25
Sererim <danielleymanw@gmail.com>
2023-12-16 13:50:20 +0300

    Reorganised the project structure for better work.

--- src/compare_averages_of_list.py
+++ src/compare_averages_of_list.py
@@ -1,9 +1,15 @@
+"""Compares two lists of integers using their average value.
+    Average value calculated as:
+    Sum of all numbers in a list, devided by the length of the list.
+
+    Returns:
+        Three possible messages that are kept in msgs variable.
+"""
 msgs: tuple[str] = (
     "Первый список имеет большее среднее значение", # Если среднее значение первого списка больше
     "Второй список имеет большее среднее значение", # Если среднее значение второго списка больше
     "Средние значения равны"                        # Если средние значения списков равны
 )
-
 
 def find_average(lst: list[int]) -> int:
     """Function for finding an average value of a list.
@@ -16,23 +22,25 @@ def find_average(lst: list[int]) -> int:
         average = (1 + 2 + 3 + 4) / 4 = 2.5
     """
     num: int = 0
-    
     for i in lst:
         num += i
     num /= len(lst)
-    
     return num
 
+def compare(lst_1: list[int], lst_2: list[int]) -> str:
+    """Function that compares two lists using their averages.
+    Args:
+        lst_1 (list[int]): First list.
+        lst_2 (list[int]): Second list.
 
-def compare(lst_1: list[int], lst_2: list[int]) -> eval:
-    global msgs
+    Returns:
+        str: message from msgs variable. 
+    """
     message: int = 0
-    
-    if (find_average(lst_1) < find_average(lst_2)):
+    if find_average(lst_1) < find_average(lst_2):
         message = 1
-    elif(find_average(lst_1) == find_average(lst_2)):
+    elif find_average(lst_1) == find_average(lst_2):
         message = 2
-    
     return msgs[message]
 
 if __name__ == "__main__":
---
+++ test/tests.py
@@ -0,0 +1,16 @@
+from src.compare_averages_of_list import (
+    compare, find_average, msgs)
+
+def test_average() -> None:
+    assert find_average([2, 2]) == 2
+    assert find_average([10, 10]) == 10
+    assert find_average([-1, 1]) == 0
+
+def test_compare_message_one() -> None:
+    assert compare([1, 2], [1]) == msgs[0]
+
+def test_compare_message_two() -> None:
+    assert compare([1, 1, 1], [1, 3]) == msgs[1]
+
+def test_compare_message_three() -> None:
+    assert compare([1, 1], [1, 1]) == msgs[2]
