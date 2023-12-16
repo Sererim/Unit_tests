eb66ab9479fae99de6c2568b096aef19fcab851d
Sererim <danielleymanw@gmail.com>
2023-12-16 13:57:16 +0300

    Added docstrings and renamed the folder.

--- tests/tests.py
+++ tests/tests.py
@@ -2,15 +2,27 @@ from src.compare_averages_of_list import (
     compare, find_average, msgs)
 
 def test_average() -> None:
+    """
+    Testing work of average function.
+    """
     assert find_average([2, 2]) == 2
     assert find_average([10, 10]) == 10
     assert find_average([-1, 1]) == 0
 
 def test_compare_message_one() -> None:
+    """
+    Testing work of the first stream flow.
+    """
     assert compare([1, 2], [1]) == msgs[0]
 
 def test_compare_message_two() -> None:
+    """
+    Testing work of the second stream flow.
+    """
     assert compare([1, 1, 1], [1, 3]) == msgs[1]
 
 def test_compare_message_three() -> None:
+    """
+    Testing work of the third stream flow.
+    """
     assert compare([1, 1], [1, 1]) == msgs[2]
